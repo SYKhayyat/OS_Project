5109ee197e71ba3dbf15a49484b69cd421450809
Benjamin Yazdi <b.m.yazdi@wustl.edu>
2026-03-10 12:10:40 -0400

    Add submitCompletion method to Slave interface
    Add submitCompletion method to Slave interface for job identification.

--- src/Slave.java
+++ src/Slave.java
@@ -1,4 +1,6 @@
 public interface Slave {
     void acceptJob();
     void doJob();
+    String submitCompletion(); /* The client’s submission should include the type, and an ID number that 
+    will be used to identify the job throughout the system. */
 }
